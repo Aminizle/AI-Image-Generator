283fee362328c9b6f7bb64e465ee6bf615fd66d3
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-02-04 13:31:33 -0500

    added components

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+import { Loader, Card, FormField } from "../components";
 
 function Home() {
+  const [loading, setLoading] = useState(false);
+  const [allPosts, setAllPosts] = useState(null);
+
   return (
-    <div>Home</div>
-  )
+    <section className="max-w-7xl">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          The Community Showcase
+        </h1>
+      </div>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
