1eb1e3b9b0c1b82ab6d505bdf8f8c340a0842ee8
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-02-05 17:20:57 -0500

    added formfield style

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
 
   const RenderCards = ({ data, title }) => {
     if (data?.length > 0) {
-      return data.map((post) => <Card keyu={post._id} {...post} />);
+      return data.map((post) => <Card key={post._id} {...post} />);
     }
     return (
       <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">
